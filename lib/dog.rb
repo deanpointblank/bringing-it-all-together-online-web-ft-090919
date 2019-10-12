98f34bb6bf5961991258f260062c0da9aabd03b7
Dean L Betty <deanlloyd64@gmail.com>
2019-10-12 16:53:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -99,7 +99,7 @@ class Dog
       WHERE name = ?
     SQL
     
-    DB[:conn].execute(sql, id).map do |row|
+    DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
     end.first
     
