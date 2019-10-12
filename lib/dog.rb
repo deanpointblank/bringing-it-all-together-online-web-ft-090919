65f5702e9b21ef5a6ccccb8dc3d1fea28ffab565
Dean L Betty <deanlloyd64@gmail.com>
2019-10-12 16:13:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -70,8 +70,8 @@ class Dog
   
   def self.create(name:, breed:)
     new_dog = self.new
-    new_dog.name = name
-    new_dog.breed = breed
+    @name = name
+    @breed = breed
     new_dog.save
   end
   
